06f4afd1837bae6a25d04fa31a362819982bc992
adarshmaharjan <40669144+adarshmaharjan@users.noreply.github.com>
2021-07-12 18:41:38 +0545

    [created] projects page after fetching repo url

---
+++ projects/projects.js
@@ -0,0 +1,24 @@
+//fetch github users
+
+fetch('https://api.github.com/users/adarshmaharjan')
+	.then((response) => response.json())
+	.then(function (data) {
+		//fetch github repo
+		const repoURL = data['repos_url'];
+		// console.log(repoURL);
+		//fetching
+		fetch(repoURL)
+			.then((response) => response.json())
+			.then((repository) => {
+				// console.log(repository);
+				const githubRepo = document.getElementById(
+					'repository-container'
+				);
+				githubRepo.innerHTML = repository.map((items) => {
+					const { name } = items;
+					// console.log(name);
+					return `<li>${name}</li>`;
+				});
+			});
+	});
+//fetch github repo
